a444a5257d5990b5384e0014074af13c2ad2541a
mairitikk <mairi.tikk@gmail.com>
2026-03-19 19:57:58 +0100

    front end changes

--- back_end/index.js
+++ back_end/index.js
@@ -1,15 +1,15 @@
 // Carga de protocolo http
-const { createServer } = require('http');
+const { createServer } = require("http");
 
 // Carga de aplicación de express
-const app = require('./src/app');
+const app = require("./src/app");
 
 // Carga de variables de entorno
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 // Configuracion de bbdd
-require('./src/config/db');
+require("./src/config/db");
 
 // Creación del servidor
 const server = createServer(app);
@@ -21,5 +21,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT);
 
 // Handler de eventos del servidor
-server.on('listening', () => console.log(`Server running on port: ${PORT}`));
-server.on('error', (error) => console.log(error));
+server.on("listening", () => console.log(`Server running on port: ${PORT}`));
+server.on("error", (error) => console.log(error));
--- back_end/src/routes/api.js
+++ back_end/src/routes/api.js
@@ -1,11 +1,10 @@
 //const { checkToken } = require('../middlewares/auth.middleware');
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 // Rutas
 //router.use('/todos', require('./api/todos'));
 
 //router.use('/users', require('./api/users'));
-
 
 module.exports = router;
